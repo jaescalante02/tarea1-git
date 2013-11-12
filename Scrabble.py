399950737eb017fa72c510a780942a3c2587a826
Juan Escalante <jaescalante02@JuanLinuxEQP.(none)>
2013-11-12 17:58:42 -0430

    Inclusión de las respuestas, arreglos de interfaz

---
+++ Scrabble.py
@@ -0,0 +1,24 @@
+#Programa que revisa cuales letras no se repiten de manera continua en
+#una lista de palabras
+#Elaborado por: Juan A. Escalante (10-10227)
+#Para Devlabs-USB Tarea 1
+
+import sys
+archivo  = open ("sowpods.txt")
+archivo = archivo.read()
+
+def main():
+    result = []
+    # 65 hasta 90: Valores ASCII de las letras mayusculas.
+    for x in range(65,91):    
+        if(not( 2*(unichr(x)) in archivo)):
+            result.append(unichr(x))   
+
+    if(result):
+        print 'Las letras que no se encuentran repetidas de manera continua son:'
+        for letra in result:
+            print letra
+    else:
+        print 'Todas las letras se repiten de manera continua al menos una vez'
+
+main()
